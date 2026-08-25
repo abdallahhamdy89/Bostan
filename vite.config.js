acce5e03a3497004c9d7e0e14497da756fbe7101
Abdallah Moustafa <mr.abdallah.moustafa@gmail.com>
2026-08-25 16:42:27 +0000

    Configure production UI access

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    allowedHosts: ["bostan.ddns.net"],
+
     proxy: {
       "/api": "http://localhost:3001",
     },
